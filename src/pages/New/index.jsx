c3060ccd87d829e1e95de3bdb086695a424dfa2b
Alex <alexxcamargo1000@gmail.com>
2022-08-18 18:32:15 -0300

    feat: create input

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,9 +1,13 @@
+import { Header } from "../../components/Header";
 import { Container } from "./styles";
 
 export function New() {
   return (
     <Container>
-      
+      <Header />
+      <main>
+        
+      </main>
     </Container>
   )
 }
