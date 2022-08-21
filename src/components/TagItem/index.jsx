667234a5a9dcfff57a3ff6feb33e7c5484097d78
Alex <alexxcamargo1000@gmail.com>
2022-08-21 10:42:28 -0300

    feat: create structure for page preview

--- src/components/TagItem/index.jsx
+++ src/components/TagItem/index.jsx
@@ -1,9 +1,5 @@
+import { Container } from "./styles";
 
-
-export function TagItem({title, ...rest}) {
-  return (
-    <Container {...rest}>
-      {title}
-    </Container>
-  )
+export function TagItem({ title, ...rest }) {
+  return <Container {...rest}>{title}</Container>;
 }
--- src/components/TagItem/styles.js
+++ src/components/TagItem/styles.js
@@ -1,4 +1,13 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-`
+  width: fit-content;
+  padding: 0.8rem 1.6rem;
+  background-color: ${({ theme, tagColor }) =>
+    !tagColor ? theme.COLORS.TEXT_IN_PRIMARY : `#282124`};
+    
+  color: #e5e5e5;
+  font-size: 12px;
+  line-height: 14px;
+  border-radius: 8px;
+`;
