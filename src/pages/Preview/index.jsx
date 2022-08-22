056c614c94d9cd22dd5047190ac2ec13fb9ef55b
Alex <alexxcamargo1000@gmail.com>
2022-08-22 10:56:51 -0300

    feat: update styles page preview

--- src/pages/Preview/index.jsx
+++ src/pages/Preview/index.jsx
@@ -2,49 +2,47 @@ import { Header } from "../../components/Header";
 import { TagItem } from "../../components/TagItem";
 import { ArrowLeft, Star, Clock } from "phosphor-react";
 
-import { Container } from "./styles";
+import { Container, TitleWrapper, Title, InfoUser, TagsWrapper, Content } from "./styles";
 
 export function Preview() {
   return (
     <Container>
       <Header />
       <main>
-        <div className="content_title">
+        <TitleWrapper>
           <a href="/">
             <ArrowLeft size={19} />
             Voltar
           </a>
-          <div className="title">
+          <Title>
             <h1>Interestellar</h1>
             <div className="rating">
-              <Star className="active" size={16} />
-              <Star className="active" size={16} />
-              <Star className="active" size={16} />
-              <Star className="active" size={16} />
-              <Star className="active" size={16} />
+              <Star className="active" />
+              <Star className="active" />
+              <Star className="active" />
+              <Star className="active" />
+              <Star className="" />
             </div>
-          </div>
+          </Title>
 
-          <div className="sub">
+          <InfoUser>
             <img
               src="https://github.com/alexxcamargo1000.png"
               alt="Foto de perfil"
             />
             <span>Por Alex Camargo</span>
-            <span>
-              <Clock size={16} />
-              23/05/22 às 08:00
-            </span>
-          </div>
+            <Clock size={16} />
+            <span>23/05/22 às 08:00</span>
+          </InfoUser>
 
-          <div className="Tags">
+          <TagsWrapper>
             <TagItem tagColor title="Ficção Científica" />
             <TagItem tagColor title="Ficção Científica" />
             <TagItem tagColor title="Ficção Científica" />
-          </div>
-        </div>
+          </TagsWrapper>
+        </TitleWrapper>
 
-        <div className="content_text">
+        <Content>
           <p>
             Pragas nas colheitas fizeram a civilização humana regredir para uma
             sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto
@@ -57,28 +55,29 @@ export function Preview() {
             NASA liderada pelo professor John Brand. O cientista revela que um
             buraco de minhoca foi aberto perto de Saturno e que ele leva a
             planetas que podem oferecer condições de sobrevivência para a
-            espécie humana. As "missões Lázaro" enviadas anos antes
+            espécie humana. 
+            <br/><br/>
+            As "missões Lázaro" enviadas anos antes
             identificaram três planetas potencialmente habitáveis orbitando o
             buraco negro Gargântua: Miller, Edmunds e Mann – nomeados em
             homenagem aos astronautas que os pesquisaram. Brand recruta Cooper
             para pilotar a nave espacial Endurance e recuperar os dados dos
             astronautas; se um dos planetas se mostrar habitável, a humanidade
             irá seguir para ele na instalação da NASA, que é na realidade uma
-            enorme estação espacial. A partida de Cooper devasta Murphy.
-
-            Além de Cooper, a tripulação da Endurance é formada pela bióloga
-            Amelia, filha de Brand; o cientista Romilly, o físico planetário
-            Doyle, além dos robôs TARS e CASE. Eles entram no buraco de minhoca
-            e se dirigem a Miller, porém descobrem que o planeta possui enorme
-            dilatação gravitacional temporal por estar tão perto de Gargântua:
-            cada hora na superfície equivale a sete anos na Terra. Eles entram
-            em Miller e descobrem que é inóspito já que é coberto por um oceano
-            raso e agitado por ondas enormes. Uma onda atinge a tripulação
-            enquanto Amelia tenta recuperar os dados de Miller, matando Doyle e
-            atrasando a partida. Ao voltarem para a Endurance, Cooper e Amelia
-            descobrem que 23 anos se passaram.
+            enorme estação espacial. A partida de Cooper devasta Murphy. Além de
+            Cooper, a tripulação da Endurance é formada pela bióloga Amelia,
+            filha de Brand; o cientista Romilly, o físico planetário Doyle, além
+            dos robôs TARS e CASE. Eles entram no buraco de minhoca e se dirigem
+            a Miller, porém descobrem que o planeta possui enorme dilatação
+            gravitacional temporal por estar tão perto de Gargântua: cada hora
+            na superfície equivale a sete anos na Terra. Eles entram em Miller e
+            descobrem que é inóspito já que é coberto por um oceano raso e
+            agitado por ondas enormes. Uma onda atinge a tripulação enquanto
+            Amelia tenta recuperar os dados de Miller, matando Doyle e atrasando
+            a partida. Ao voltarem para a Endurance, Cooper e Amelia descobrem
+            que 23 anos se passaram.
           </p>
-        </div>
+        </Content>
       </main>
     </Container>
   );
--- src/pages/Preview/styles.js
+++ src/pages/Preview/styles.js
@@ -10,6 +10,7 @@ export const Container = styled.div`
     overflow-y: auto;
     width: min(112rem, 90%);
     margin: 0 auto;
+    padding-bottom: 1rem;
 
     &::-webkit-scrollbar {
       width: 1rem;
@@ -21,6 +22,84 @@ export const Container = styled.div`
       background: ${({ theme }) => theme.COLORS.PRIMARY};
       border-radius: 32px;
     }
-    
+  }
+`;
+
+export const TitleWrapper = styled.div`
+  margin-top: 4rem;
+
+  > a {
+    display: flex;
+    color: ${({ theme }) => theme.COLORS.PRIMARY};
+    width: fit-content;
+    gap: 8px;
+  }
+
+  img {
+    width: 1.6rem;
+    height: 1.6rem;
+    border-radius: 0.8rem;
+    border: 1px solid ${({ theme }) => theme.COLORS.BORDER_IN_BG};
+  }
+`;
+
+export const Title = styled.div`
+  display: flex;
+  align-items: stretch;
+  gap: 1.9rem;
+  margin-bottom: 2.4rem;
+
+  h1 {
+    font-weight: 500;
+    font-size: 36px;
+    line-height: 47px;
+  }
+
+  .rating {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+
+    .active path {
+      fill: ${({ theme }) => theme.COLORS.PRIMARY};
     }
-`
+
+    svg {
+      width: 1.875rem;
+      height: 1.875rem;
+      color: ${({ theme }) => theme.COLORS.PRIMARY};
+    }
+  }
+`;
+
+export const InfoUser = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  margin-bottom: 4rem;
+
+  > span {
+    font-size: 1.6rem;
+    line-height: 1.9rem;
+    color: ${({ theme }) => theme.COLORS.TITLE};
+  }
+
+  > svg {
+    color: ${({ theme }) => theme.COLORS.PRIMARY};
+  }
+`;
+
+export const TagsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  margin-bottom: 4rem;
+`;
+
+export const Content = styled.div`
+  text-align: justify;
+  color: ${({ theme }) => theme.COLORS.TITLE};
+  font-size: 1.6rem;
+  line-height: 2.1rem;
+  padding: 8px;
+`;
